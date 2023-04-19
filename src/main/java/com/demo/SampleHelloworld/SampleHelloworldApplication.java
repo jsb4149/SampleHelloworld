3b97fd221d4d14c87cc1b5b9b129ad15f157d2c6
muarala <jannu.deekshitha123@gmail.com>
2023-04-19 17:17:30 +0530

    first commit

---
+++ src/main/java/com/demo/SampleHelloworld/SampleHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.demo.SampleHelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleHelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/SampleHelloworld/SampleHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.SampleHelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
